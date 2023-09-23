543f5dcaef20680deca4f7378cbc0e88bc5cec3f
gabriel.francisco <gabrielfranciscodev@hotmail.com>
2023-09-23 14:15:29 -0300

    Initial commit

---
+++ src/main/java/br/com/pricealertws/PriceAlertWsApplication.java
@@ -0,0 +1,13 @@
+package br.com.pricealertws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceAlertWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceAlertWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pricealertws/PriceAlertWsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pricealertws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceAlertWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
